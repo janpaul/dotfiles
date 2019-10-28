33bdc7ac2cccf78e0621c3a5cdf3f64b9f504ce1
JP <janpaul@elidon.net>
2019-10-28 11:44:36 +0100

    adds handbrake

--- bootstrap.sh
+++ bootstrap.sh
@@ -127,7 +127,7 @@ brew_cask google-chrome firefox
 brew_cask visual-studio-code intellij-idea dash
 brew_cask signal whatsapp slack zoomus
 brew_cask tower transmit itsycal docker
-brew_cask iina vlc transmission
+brew_cask iina vlc transmission handbrake
 brew_cask scummvm
 brew_cask install blender
 
