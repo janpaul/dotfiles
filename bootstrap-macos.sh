5e45427223809706bfa93402bee95c19e89831c7
janpaul <janpaul@elidon.net>
2020-09-06 11:20:12 +0200

    adds plex

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -147,7 +147,7 @@ brew_cask visual-studio-code intellij-idea
 brew_cask signal whatsapp telegram
 brew_cask slack
 brew_cask tower transmit github
-brew_cask iina transmission handbrake youtube-to-mp3
+brew_cask iina transmission handbrake youtube-to-mp3 plex
 brew_cask scummvm steam
 brew_cask blender
 brew_cask anaconda miniconda
