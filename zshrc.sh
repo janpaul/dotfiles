99e7ceb040214defd16b398eda07c9503bf10f69
janpaul <janpaul@elidon.net>
2022-01-12 16:28:25 +0100

    adds thefuck+scala plugins

--- zshrc.sh
+++ zshrc.sh
@@ -19,7 +19,7 @@ export UPDATE_ZSH_DAYS=13
 ENABLE_CORRECTION="true"
 COMPLETION_WAITING_DOTS="true"
 DISABLE_UNTRACKED_FILES_DIRTY="true"
-plugins=(brew docker git gitignore github gitignore history rust node npm macos tmux vscode xcode yarn vscode nvm)
+plugins=(brew docker git gitignore github gitignore history rust node npm macos tmux vscode xcode yarn vscode nvm thefuck scala)
 source $ZSH/oh-my-zsh.sh
 
 export ARCHFLAGS="-arch x86_64"
