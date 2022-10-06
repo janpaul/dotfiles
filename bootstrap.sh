c33ebd0f3c4e0a9bbb1e82c6dda6485d27dfcdd3
janpaul <janpaul@hey.com>
2022-10-06 19:02:41 +0200

    updates node versions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 #
 
-NODE_STABLE=12
-NODE_LATEST=14
+NODE_STABLE=16
+NODE_LATEST=18
 
 if [ $(id -u) -eq 0 ]; then
     echo "don't run this script as root or under sudo. It can and will mess up your homedir."
