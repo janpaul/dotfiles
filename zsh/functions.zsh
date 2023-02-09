4e65478f0fd54f689c6140fbb295004d394760f3
janpaul <janpaul@hey.com>
2023-02-09 13:50:25 +0100

    adds yt-dlp shortcut

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,12 +1,12 @@
 #
 # update
 function ,update() {
-brew update && brew upgrade && brew upgrade --cask && brew cleanup
-tldr --update
-rustup update
-nvm install 16
-nvm install 18
-nvm install 19
+    brew update && brew upgrade && brew upgrade --cask && brew cleanup
+    tldr --update
+    rustup update
+    nvm install 16
+    nvm install 18
+    nvm install 19
 }
 
 #
@@ -15,3 +15,9 @@ alias pushm="git push origin master"
 alias pushd="git push origin development"
 alias pullm="git fetch origin && git merge origin/master"
 alias pulld="git fetch origin && git merge origin/development"
+
+#
+# yt-dlp
+function ,yt() {
+    yt-dlp --extract-audio --audio-format mp3 --embed-thumbnail -o "%(title)s %(upload_date)s.mp3" "$@"
+}
