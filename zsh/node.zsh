ab79a378ebcf159826f119c06aec8af8bb106579
Jan Paul <janpaul@hey.com>
2023-07-17 10:56:33 +0200

    node 18 by default

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -8,20 +8,8 @@ export NVM_DIR=~/.nvm
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm "$@"
 }
-node() {
-unset -f node
-export NVM_DIR=~/.nvm
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-node "$@"
-}
-npm() {
-unset -f npm
-export NVM_DIR=~/.nvm
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-npm "$@"
-}
 
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm use 18
 
 #
 # bun
