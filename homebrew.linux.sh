c47718041b09d6b977255b8a534b6da20453cf5a
Jan Paul <janpaul@elidon.net>
2025-10-07 10:18:07 +0200

    adds brews for linux

---
+++ homebrew.linux.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/zsh
+
+## core unixy stuff
+brew install git zsh vim
+## zsh
+brew install zsh-syntax-highlighting zsh-autosuggestions zsh-completions zoxide fzf starship
+## utilities
+brew install thefuck the_silver_searcher tldr curl wget telnet jq p7zip wtf htop calc cointop gnupg watchman git-delta diff-so-fancy bat fd gh ripgrep neofetch netcat lsd
+## neovim
+brew install neovim
+## Node
+brew install pnpm
+## Haskell
+brew install ghc cabal-install
+## Python
+brew install python
+## JVM
+brew install kotlin scala leiningen gradle maven sbt
+## C / C++
+brew install gcc global cmake boost nasm vcpkg folly
+## Wasm
+brew install emscripten binaryen
+## images, video, ...
+brew install ffmpeg imagemagick yt-dlp atomicparsley
+## 6502
+brew install cc65
+## OSM /maps
+brew install osmosis
+## Funny stuff
+brew install cmatrix lolcat nyancat sl
