46e5491df12842ab67c23961224f8bd0677a05b5
Jan Paul <janpaul@elidon.net>
2026-04-20 09:11:21 +0200

    adds vcpkg

--- zshrc.macos.sh
+++ zshrc.macos.sh
@@ -14,6 +14,8 @@ PATH="${HOMEBREW}/opt/curl/bin:${HOME}/code/dotfiles/bin.macos:$PATH:"
 # Java on Mac
 export JAVA_HOME="/Applications/IntelliJ IDEA.app/Contents/jbr/Contents/Home"
 
+export VCPKG_ROOT="$HOME/code/vcpkg"
+
 export PATH
 
 # shellcheck disable=all
