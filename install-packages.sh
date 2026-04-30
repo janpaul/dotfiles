b340315b699a12d12cf89de14aebb287747cc397
Jan Paul <janpaul@elidon.net>
2026-04-30 11:50:13 +0200

    check for homebrew in postinstall

--- install-packages.sh
+++ install-packages.sh
@@ -1,3 +1,8 @@
+if ! command -v brew >/dev/null 2>&1; then
+    echo "Homebrew not installed. Run postinstall.sh first."
+    exit 1
+fi
+
 SCRIPT_DIR="${HOME}/code/dotfiles"
 brew bundle --file="${SCRIPT_DIR}/Brewfile"
 
